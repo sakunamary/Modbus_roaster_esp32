83984eb569463576ae68e79018dc92f1292317e9
Tim <sakunamary@gmail.com>
2023-01-30 15:27:18 +0800

    update

--- src/main.cpp
+++ src/main.cpp
@@ -450,8 +450,6 @@ if (user_wifi.Init_mode)
     mb.Hreg(FAN_HREG,0);  //初始化赋值
 
    Serial.println("Modbus-TCP  started");  
-
-
     timestamp = millis();
 }
 
@@ -515,7 +513,7 @@ void loop()
        } else {
             heat_from_enc = encoder.getCount();//设置counter为赋值
         }
-        
+
        heat_from_Hreg = heat_from_enc; //手动模式下，同步数据到寄存器
        mb.Hreg(HEAT_HREG,heat_from_Hreg); //手动模式下，写入寄存器
        pwm.write(HEAT_PIN, map(heat_from_enc,0,100,0,4096), frequency, resolution); //自动模式下，将heat数值转换后输出到pwm
@@ -523,10 +521,10 @@ void loop()
 
        //FAN  控制部分 
         fan_from_analog = analogRead(FAN_IN);   //获取模拟量信息
-        
         fan_from_Hreg = map(fan_from_analog,0,1024,0,100); // 模拟量 1024 转为 100 
         mb.Hreg(FAN_HREG,fan_from_Hreg);//手动模式下，写入寄存器
-        pwm.write(FAN_PIN,map(fan_from_Hreg,0,100,0,4096), frequency, resolution);//手动模式下，将fan数值输出到pwm
+        pwm.write(FAN_PIN,map(fan_from_Hreg,0,100,0,4096), frequency, resolution);//手动模式下，将fan数值输出到pwm     
+
     }
 
    checkLowPowerMode(BT_AvgTemp); //测量是否进入睡眠模式
