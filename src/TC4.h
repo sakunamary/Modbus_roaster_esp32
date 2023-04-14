f5d98f86ef5b0b15dc1a29bffb6b328aad6e9f5e
Tim <sakunamary@gmail.com>
2023-04-14 17:32:35 +0800

    update

--- src/TC4.h
+++ src/TC4.h
@@ -88,9 +88,9 @@ const char wifi_sussce_html[] PROGMEM = R"rawliteral(
 )rawliteral";
 
 
-//有roll 和 风压传感器
 
-#if defined(ROLL_CONTROL) &&  defined(HAS_AP_INPUT)
+//有风压有roll
+#if defined(HAS_AP_INPUT) && defined(ROLL_CONTROL)
 
 const char index_html[] PROGMEM = R"rawliteral(
 <!doctype html><html lang='cn'>
@@ -180,11 +180,6 @@ const char index_html[] PROGMEM = R"rawliteral(
                 </div>
                 <br/>
                 <div class='form-floating'>
-                <label>风压传感器CanID (当前ID:%air_msgID_compens%)</label>
-                <input type='text'  class='form-control' name='air_msgID'> 
-                </div>
-                <br/>
-                <div class='form-floating'>
                     <label>热源PWM频率 (当前Hz:%HEAT_PWM_compens%)</label>
                     <input type='number' step = '100' max = '3000' min='0' class='form-control' name='HEAT_PWM'> 
                     </div>
@@ -193,7 +188,7 @@ const char index_html[] PROGMEM = R"rawliteral(
                     <label>风门PWM频率 (当前Hz:%FAN_PWM_compens%)</label>
                     <input type='number'  step = '100' max = '3000' min='0'class='form-control' name='FAN_PWM'> 
                     </div>
-                    <br/>                    
+                    <br/>    
                 <div class='form-floating'>
                     <label>滚桶PWM频率 (当前Hz:%ROLL_PWM_compens%)</label>
                     <input type='number'  step = '100' max = '3000' min='0' class='form-control' name='ROLL_PWM'> 
@@ -221,270 +216,133 @@ const char index_html[] PROGMEM = R"rawliteral(
     </main> 
 </body></html>
 )rawliteral";
-
-#endif 
-
-
-// 只有roll 
-#else 
-#if defined(ROLL_CONTROL) 
-
-const char index_html[] PROGMEM = R"rawliteral(
-<!doctype html><html lang='cn'>
-<head>
-<script>
-  function submitMessage() {
-    alert("数据已保存");
-    setTimeout(function(){ document.location.reload(false); }, 500);
-  }
-</script>
-    <meta charset='utf-8'><meta name='viewport' content='width=device-width, initial-scale=1'>
-    <title>MODBUS CONTROLER SETTING</title>
-    <style>*,::after,::before{box-sizing:border-box;}
-    body{margin:0;font-family:'Segoe UI',Roboto,'Helvetica Neue',Arial,'Noto Sans','Liberation Sans';
-    font-size:1rem;
-    font-weight:400;
-    line-height:1.5;color:#212529;
-    background-color:#f5f5f5;}
-    .form-control{
-    display:block;
-    width: 400px;
-    height:calc(1.5em + .75rem + 2px);
-    border:1px solid #ced4da;}
-    button{border:1px solid transparent;
-    color:#fff;
-    background-color:#007bff;
-    border-color:#007bff;
-    padding:.5rem 1rem;
-    font-size:1.25rem;
-    line-height:1.5;
-    border-radius:.3rem;
-    width:400px}
-    .form-signin{
-    width: 400px;
-    padding:15px;
-    margin:auto;}
-    h1,p{text-align:center}</style> 
-</head> 
-<body>
-    <main class='form-signin'> 
-        <form action='/get' method='get'>
-            <h1 class=''>MODBUS CONTROLER SETTING </h1>
-            <h2 class=''>WIFI设置 </h2>
-            <div class='form-floating'>
-            <label>SSID/WIFI名字</label>
-            <input type='text' class='form-control' name='ssid'> 
-            </div>
-            <div class='form-floating'>
-            <br/>
-            <label>PASSWORD</label>
-            <input type='password' class='form-control' name='password'>
-            </div>
-            <p>
-            提示:输入空白即恢复AP模式直链模式
-            </p>
-            <br/>
-            <button type='submit'>保存</button>
-        </form>     
-        <form action='/compens' method='get'>                
-            <br/>
-            <br/>
-            <h2 class=''>热电偶温度补偿设置</h2>
-            <div class='form-floating'>
-            <label>Bean Temp/豆温 (当前值: %bt_compens%) </label>
-            <input type='number' step = '0.01' max = '20' min='-20' class='form-control'  name='Btemp_fix'> 
-            </div>
-            <br/>
-            <div class='form-floating'>
-            <label>Env  Temp/炉温 (当前值:%et_compens%)</label>
-            <input type='number' step = '0.01' max = '20' min='-20' class='form-control' name='Etemp_fix'> 
-            </div>
-            <br/>
-            <div class='form-floating'>
-            <label>Air Presure/风压差 (当前值:%AP_compens%)</label>
-            <input type='number' step = '0.1' max = '20' min='-20' class='form-control' name='Ap_fix'> 
-            </div>
-            <br/>
-            <button type='submit'onclick="submitMessage()">保存</button>
-        </form> 
-        <form action='/canbus' method='get'> 
-            <br/>
-            <br/>
-            <h2 class=''>传感器模块设置</h2>
-            <div class='form-floating'>
-                <label>温度传感器CanID (当前ID: %thermo_msgID_compens%) </label>
-                <input type='text'  class='form-control'  name='thermo_msgID'> 
-                </div>
-                <br/>
-                <div class='form-floating'>
-                <label>风压传感器CanID (当前ID:%air_msgID_compens%)</label>
-                <input type='text'  class='form-control' name='air_msgID'> 
-                </div>
-                <br/>
-                <div class='form-floating'>
-                    <label>热源PWM频率 (当前Hz:%HEAT_PWM_compens%)</label>
-                    <input type='number' step = '100' max = '3000' min='0' class='form-control' name='HEAT_PWM'> 
-                    </div>
-                    <br/>
-                <div class='form-floating'>
-                    <label>风门PWM频率 (当前Hz:%FAN_PWM_compens%)</label>
-                    <input type='number'  step = '100' max = '3000' min='0'class='form-control' name='FAN_PWM'> 
-                    </div>
-                    <br/>                                       
-                <button type='submit'onclick="submitMessage()">保存</button>
-
-        </form>              
-        <form action='/other' method='get'>   
-            <br/>
-            <br/>
-            <div class='form-floating'>
-            <h2 class=''>其他设置</h2>  
-            <label>采样时间 (当前值: %sampling_time%) s</label>
-            <input type='number' step = '0.25' max = '4' min='0.75' class='form-control'  name='sampling_time'> 
-            </div>
-            <br/>
-  
-            <button type='submit'onclick="submitMessage()">保存</button>
-        </form> 
-            <p>
-            <a href='/update' target='_blank'>FIRMWARE UPDATE verison:%version%</a>
-            </p>
-            <br/>
-    </main> 
-</body></html>
-)rawliteral";
-#endif 
-
-
-
-// 只有 风压  
-#if defined(HAS_AP_INPUT)
-
-const char index_html[] PROGMEM = R"rawliteral(
-<!doctype html><html lang='cn'>
-<head>
-<script>
-  function submitMessage() {
-    alert("数据已保存");
-    setTimeout(function(){ document.location.reload(false); }, 500);
-  }
-</script>
-    <meta charset='utf-8'><meta name='viewport' content='width=device-width, initial-scale=1'>
-    <title>MODBUS CONTROLER SETTING</title>
-    <style>*,::after,::before{box-sizing:border-box;}
-    body{margin:0;font-family:'Segoe UI',Roboto,'Helvetica Neue',Arial,'Noto Sans','Liberation Sans';
-    font-size:1rem;
-    font-weight:400;
-    line-height:1.5;color:#212529;
-    background-color:#f5f5f5;}
-    .form-control{
-    display:block;
-    width: 400px;
-    height:calc(1.5em + .75rem + 2px);
-    border:1px solid #ced4da;}
-    button{border:1px solid transparent;
-    color:#fff;
-    background-color:#007bff;
-    border-color:#007bff;
-    padding:.5rem 1rem;
-    font-size:1.25rem;
-    line-height:1.5;
-    border-radius:.3rem;
-    width:400px}
-    .form-signin{
-    width: 400px;
-    padding:15px;
-    margin:auto;}
-    h1,p{text-align:center}</style> 
-</head> 
-<body>
-    <main class='form-signin'> 
-        <form action='/get' method='get'>
-            <h1 class=''>MODBUS CONTROLER SETTING </h1>
-            <h2 class=''>WIFI设置 </h2>
-            <div class='form-floating'>
-            <label>SSID/WIFI名字</label>
-            <input type='text' class='form-control' name='ssid'> 
-            </div>
-            <div class='form-floating'>
-            <br/>
-            <label>PASSWORD</label>
-            <input type='password' class='form-control' name='password'>
-            </div>
-            <p>
-            提示:输入空白即恢复AP模式直链模式
-            </p>
-            <br/>
-            <button type='submit'>保存</button>
-        </form>     
-        <form action='/compens' method='get'>                
-            <br/>
-            <br/>
-            <h2 class=''>热电偶温度补偿设置</h2>
-            <div class='form-floating'>
-            <label>Bean Temp/豆温 (当前值: %bt_compens%) </label>
-            <input type='number' step = '0.01' max = '20' min='-20' class='form-control'  name='Btemp_fix'> 
-            </div>
-            <br/>
-            <div class='form-floating'>
-            <label>Env  Temp/炉温 (当前值:%et_compens%)</label>
-            <input type='number' step = '0.01' max = '20' min='-20' class='form-control' name='Etemp_fix'> 
-            </div>
-            <br/>
-            <div class='form-floating'>
-            <label>Air Presure/风压差 (当前值:%AP_compens%)</label>
-            <input type='number' step = '0.1' max = '20' min='-20' class='form-control' name='Ap_fix'> 
-            </div>
-            <br/>
-            <button type='submit'onclick="submitMessage()">保存</button>
-        </form> 
-        <form action='/canbus' method='get'> 
-            <br/>
-            <br/>
-            <h2 class=''>传感器模块设置</h2>
-            <div class='form-floating'>
-                <label>温度传感器CanID (当前ID: %thermo_msgID_compens%) </label>
-                <input type='text'  class='form-control'  name='thermo_msgID'> 
-                </div>
-                <br/>
-                <div class='form-floating'>
-                <label>风压传感器CanID (当前ID:%air_msgID_compens%)</label>
-                <input type='text'  class='form-control' name='air_msgID'> 
-                </div>
-                <br/>
-                <div class='form-floating'>
-                    <label>热源PWM频率 (当前Hz:%HEAT_PWM_compens%)</label>
-                    <input type='number' step = '100' max = '3000' min='0' class='form-control' name='HEAT_PWM'> 
-                    </div>
-                    <br/>
-                <div class='form-floating'>
-                    <label>风门PWM频率 (当前Hz:%FAN_PWM_compens%)</label>
-                    <input type='number'  step = '100' max = '3000' min='0'class='form-control' name='FAN_PWM'> 
-                    </div>
-                    <br/>                                       
-                <button type='submit'onclick="submitMessage()">保存</button>
-
-        </form>              
-        <form action='/other' method='get'>   
-            <br/>
-            <br/>
-            <div class='form-floating'>
-            <h2 class=''>其他设置</h2>  
-            <label>采样时间 (当前值: %sampling_time%) s</label>
-            <input type='number' step = '0.25' max = '4' min='0.75' class='form-control'  name='sampling_time'> 
-            </div>
-            <br/>
-  
-            <button type='submit'onclick="submitMessage()">保存</button>
-        </form> 
-            <p>
-            <a href='/update' target='_blank'>FIRMWARE UPDATE verison:%version%</a>
-            </p>
-            <br/>
-    </main> 
-</body></html>
-)rawliteral";
 #endif
+
+
+//只有ROLL没有air
+#if defined(ROLL_CONTROL) && !defined(HAS_AP_INPUT)
+
+const char index_html[] PROGMEM = R"rawliteral(
+<!doctype html><html lang='cn'>
+<head>
+<script>
+  function submitMessage() {
+    alert("数据已保存");
+    setTimeout(function(){ document.location.reload(false); }, 500);
+  }
+</script>
+    <meta charset='utf-8'><meta name='viewport' content='width=device-width, initial-scale=1'>
+    <title>MODBUS CONTROLER SETTING</title>
+    <style>*,::after,::before{box-sizing:border-box;}
+    body{margin:0;font-family:'Segoe UI',Roboto,'Helvetica Neue',Arial,'Noto Sans','Liberation Sans';
+    font-size:1rem;
+    font-weight:400;
+    line-height:1.5;color:#212529;
+    background-color:#f5f5f5;}
+    .form-control{
+    display:block;
+    width: 400px;
+    height:calc(1.5em + .75rem + 2px);
+    border:1px solid #ced4da;}
+    button{border:1px solid transparent;
+    color:#fff;
+    background-color:#007bff;
+    border-color:#007bff;
+    padding:.5rem 1rem;
+    font-size:1.25rem;
+    line-height:1.5;
+    border-radius:.3rem;
+    width:400px}
+    .form-signin{
+    width: 400px;
+    padding:15px;
+    margin:auto;}
+    h1,p{text-align:center}</style> 
+</head> 
+<body>
+    <main class='form-signin'> 
+        <form action='/get' method='get'>
+            <h1 class=''>MODBUS CONTROLER SETTING </h1>
+            <h2 class=''>WIFI设置 </h2>
+            <div class='form-floating'>
+            <label>SSID/WIFI名字</label>
+            <input type='text' class='form-control' name='ssid'> 
+            </div>
+            <div class='form-floating'>
+            <br/>
+            <label>PASSWORD</label>
+            <input type='password' class='form-control' name='password'>
+            </div>
+            <p>
+            提示:输入空白即恢复AP模式直链模式
+            </p>
+            <br/>
+            <button type='submit'>保存</button>
+        </form>     
+        <form action='/compens' method='get'>                
+            <br/>
+            <br/>
+            <h2 class=''>热电偶温度补偿设置</h2>
+            <div class='form-floating'>
+            <label>Bean Temp/豆温 (当前值: %bt_compens%) </label>
+            <input type='number' step = '0.01' max = '20' min='-20' class='form-control'  name='Btemp_fix'> 
+            </div>
+            <br/>
+            <div class='form-floating'>
+            <label>Env  Temp/炉温 (当前值:%et_compens%)</label>
+            <input type='number' step = '0.01' max = '20' min='-20' class='form-control' name='Etemp_fix'> 
+            </div>
+            <br/>
+            <button type='submit'onclick="submitMessage()">保存</button>
+        </form> 
+        <form action='/canbus' method='get'> 
+            <br/>
+            <br/>
+            <h2 class=''>传感器模块设置</h2>
+            <div class='form-floating'>
+                <label>温度传感器CanID (当前ID: %thermo_msgID_compens%) </label>
+                <input type='text'  class='form-control'  name='thermo_msgID'> 
+                </div>
+                <br/>
+                <div class='form-floating'>
+                    <label>热源PWM频率 (当前Hz:%HEAT_PWM_compens%)</label>
+                    <input type='number' step = '100' max = '3000' min='0' class='form-control' name='HEAT_PWM'> 
+                    </div>
+                    <br/>
+                <div class='form-floating'>
+                    <label>风门PWM频率 (当前Hz:%FAN_PWM_compens%)</label>
+                    <input type='number'  step = '100' max = '3000' min='0'class='form-control' name='FAN_PWM'> 
+                    </div>
+                    <br/>    
+                <div class='form-floating'>
+                    <label>滚桶PWM频率 (当前Hz:%ROLL_PWM_compens%)</label>
+                    <input type='number'  step = '100' max = '3000' min='0' class='form-control' name='ROLL_PWM'> 
+                    </div>
+                    <br/>                                            
+                <button type='submit'onclick="submitMessage()">保存</button>
+        </form>              
+        <form action='/other' method='get'>   
+            <br/>
+            <br/>
+            <div class='form-floating'>
+            <h2 class=''>其他设置</h2>  
+            <label>采样时间 (当前值: %sampling_time%) s</label>
+            <input type='number' step = '0.25' max = '4' min='0.75' class='form-control'  name='sampling_time'> 
+            </div>
+            <br/>
+  
+            <button type='submit'onclick="submitMessage()">保存</button>
+        </form> 
+            <p>
+            <a href='/update' target='_blank'>FIRMWARE UPDATE verison:%version%</a>
+            </p>
+            <br/>
+    </main> 
+</body></html>
+)rawliteral";
+#endif 
+
+
 
 
 
@@ -572,8 +430,13 @@ const char index_html[] PROGMEM = R"rawliteral(
             <h2 class=''>传感器模块设置</h2>
             <div class='form-floating'>
                 <label>温度传感器CanID (当前ID: %thermo_msgID_compens%) </label>
-                <input type='text'  class='form-control'  name='thermo_msgID'> 
-                </div>
+                <select  class='form-control'  name='thermo_msgID'> 
+                    <option value="0x0F6"> 0x0F6  K型-MAX6675 2路</option>
+                    <option value="0x0F7"> 0x0F7  K型-MAX6675 4路</option>
+                    <option value="0x0F8"> 0x0F8  PT00-MAX31865 2路</option>
+                    <option value="0x0F9"> 0x0F9  PT00-MAX31865 4路</option>
+                    </select>
+            </div>
                 <br/>
                 <div class='form-floating'>
                     <label>热源PWM频率 (当前Hz:%HEAT_PWM_compens%)</label>
